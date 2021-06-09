7770b5e889dffdf01e939d465936050c16f548ce
Jason McKenzie <jmac.atx@gmail.com>
2021-06-09 13:42:17 -0500

    add file

---
+++ store_item.rb
@@ -0,0 +1,12 @@
+#  Use hashes with symbols to represent the following scenario:
+# You are the owner of a store that sells items (you decide what specifically). Each item has various properties such as color, price, etc.
+# Represent 3 items using hashes. Each hash should have the same keys with different values.
+# Be sure to use symbols for the keys. Try creating hashes using both types of hash symbol syntaxes. (Ruby syntax {:a => 123} vs. “JavaScript” syntax {a: 123}).
+
+synth1 = {:brand => "Roland", :polyphonic => true, :model => "Juno-106"}
+synth2 = {:brand => "Korg", :polyphonic => false, :model => "MS-20"}
+synth3 = {:brand => "Novation", :polyphonic => true, :model => "Peak"}
+
+synth1 = {brand: "Roland", polyphonic: true, model: "Juno-106"}
+synth2 = {brand: "Korg", polyphonic:  false, model: "MS-20"}
+synth3 = {brand: "Novation", polyphonic: true, model: "Peak"}
